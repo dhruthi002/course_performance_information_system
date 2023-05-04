21d50dfc18a94cb4a2a373c471ccc25d3fd63207
dhruthi002 <96442315+dhruthi002@users.noreply.github.com>
2023-05-05 00:53:20 +0530

    Create db_conn.py

---
+++ db_conn.py
@@ -0,0 +1,18 @@
+import cx_Oracle
+
+cx_Oracle.init_oracle_client(lib_dir=r"path\to\oracle\21c\binaries")
+dsn_tns = cx_Oracle.makedsn('localhost', '1521', service_name='xe') 
+conn = cx_Oracle.connect(user=r'system', password='<redacted>', dsn=dsn_tns)
+cursor = conn.cursor()
+
+# if needed, place an 'r' before any parameter in order to address special characters such as '\'.
+dsn_tns = cx_Oracle.makedsn('localhost', '1521', service_name='xe') 
+conn = cx_Oracle.connect(user=r'system', password='pul1y0g0r3', dsn=dsn_tns)
+
+c = conn.cursor()
+c.execute(<query statement>) # use triple quotes if you want to spread your query across multiple lines
+for row in c:
+    #actions
+    pass
+
+  conn.close()
