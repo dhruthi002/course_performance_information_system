115b61b52861404dfdd1126ed6eec0ab454e866f
dhruthi002 <96442315+dhruthi002@users.noreply.github.com>
2023-05-05 00:53:49 +0530

    Update db_conn.py

--- db_conn.py
+++ db_conn.py
@@ -5,14 +5,11 @@ dsn_tns = cx_Oracle.makedsn('localhost', '1521', service_name='xe')
 conn = cx_Oracle.connect(user=r'system', password='<redacted>', dsn=dsn_tns)
 cursor = conn.cursor()
 
-# if needed, place an 'r' before any parameter in order to address special characters such as '\'.
-dsn_tns = cx_Oracle.makedsn('localhost', '1521', service_name='xe') 
-conn = cx_Oracle.connect(user=r'system', password='pul1y0g0r3', dsn=dsn_tns)
-
+# if needed, place an 'r' before any parameter in order to address special characters such as '\'
 c = conn.cursor()
 c.execute(<query statement>) # use triple quotes if you want to spread your query across multiple lines
 for row in c:
     #actions
     pass
 
-  conn.close()
+ conn.close()
